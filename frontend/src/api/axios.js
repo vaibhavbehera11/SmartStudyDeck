b148e8beabe58958673ac1d1142f6ac30b2f3373
Vaibhav Behera <mca-reg202527007@timscdrmumbai.in>
2026-05-05 08:02:50 +0530

    fix axios baseURL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,7 @@
-// src/api/axios.js
-
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
 });
 
 export default API;
