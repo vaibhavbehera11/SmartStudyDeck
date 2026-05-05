eb9ceaaa1a22b4937924a4cf3202d19480c2bce7
Vaibhav Behera <mca-reg202527007@timscdrmumbai.in>
2026-05-05 07:30:49 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -7,35 +7,18 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:3000"
+}));
 app.use(express.json());
-
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("MongoDB Error:", err));
 
-// Test route
 app.get("/", (req, res) => {
   res.send("API Running...");
 });
-
-
-app.get("/test-db", async (req, res) => {
-  try {
-    const Test = mongoose.model(
-      "Test",
-      new mongoose.Schema({ name: String })
-    );
-
-    const doc = await Test.create({ name: "Vaibhav" });
-
-    res.json(doc);
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-});
-
 
 const PORT = process.env.PORT || 5000;
 
@@ -50,4 +33,4 @@ app.use("/api/cards", cardRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-}); 
+});
